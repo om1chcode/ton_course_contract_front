ac8e0dbb7ef189719c4a7472a25439e308186c54
om1chcode <om1chcode@gmail.com>
2024-05-27 22:10:08 +0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
+
+const manifestUrl = "https://om1chcode.github.io/first_contract_front_end/tonconnect-manifest.json"
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>,
+)
